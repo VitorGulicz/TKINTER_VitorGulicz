9faa3436a117bb04edf2fde41754c908f6ed73b4
VitorGulicz <vdmgulicz@gmail.com>
2025-02-25 19:52:41 -0300

    update

--- Desafio1_Tela.py
+++ Desafio1_Tela.py
@@ -6,18 +6,23 @@ i.geometry('980x720+250+30')
 
 lb1=Label(i,text="Login",bg="yellow")
 #componente .grid serve tambem para posicionar utilizando indicativo de linha(row) e coluna(column)
-lb1.grid(row=1,column=1)
+#lb1.grid(row=1,column=1)
+lb1.place(x=740,y=120)
 
 lb2=Label(i,text="Senha",bg="red")
-lb2.grid(row=2,column=2)
+#lb2.grid(row=2,column=2)
+lb2.place(x=740,y=200)
 
 ed1 = Entry(i)
-ed1.grid(row=3,column=3)
+#ed1.grid(row=3,column=3)
+ed1.place(x=700,y=100)
 
 ed2 = Entry(i)
-ed2.grid(row=4,column=4)
+#ed2.grid(row=4,column=4)
+ed2.place(x=700,y=180)
 
 bt1=Button(i,text="Login")
-bt1.grid(row=5,column=5)
+#bt1.grid(row=5,column=5)
+bt1.place(x=440,y=420)
 
 i.mainloop()
